09bfad893092c30f1d03c786cdab3bf1f0c6c439
prashant153 <prashant153@gmail.com>
2018-03-06 18:58:38 +0530

    Added node chains

---
+++ NodeChains/NodeChains/Node.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace NodeChains
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+}
---
+++ NodeChains/NodeChains/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NodeChains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node first = new Node() { Value = 3 };
+            Node middle = new Node() { Value = 5 };
+            first.Next = middle;
+            Node last = new Node() { Value = 7 };
+            middle.Next = last;
+            PrintList(first);
+
+        }
+
+        private static void PrintList(Node node)
+        {
+            while(node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
