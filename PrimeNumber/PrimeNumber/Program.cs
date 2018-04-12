3ceb6fee67368bc5cd2c69787e5efbe7c2da8e73
prashant153 <prashant153@gmail.com>
2018-04-12 18:54:51 +0530

    Basic prime numbercode

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if it's Prime :");
+            int number = int.Parse(Console.ReadLine());
+            if (IsPrime(number)== true)
+            {
+                Console.WriteLine("{0} is a Prime Number.", number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Prime Number.", number);
+            }
+        }
+
+        private static bool IsPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number%i==0)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
