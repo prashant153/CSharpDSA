4c10df7f313bce6c95e70b26b471d80da946b549
prashant153 <prashant153@gmail.com>
2018-04-13 12:50:53 +0530

    Finds factorial of a number N

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of N");
+            try
+            {
+                int number =  int.Parse(Console.ReadLine());
+                Console.WriteLine(Fact(number));
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("Something went wrong!! Is your input a " +
+                                  "proper number");
+            }
+        }
+
+        private static int Fact(int number)
+        {
+            if (number==0)
+            {
+                return 1;
+            }
+            return number * Fact(number - 1);                
+        }
+    }
+}
