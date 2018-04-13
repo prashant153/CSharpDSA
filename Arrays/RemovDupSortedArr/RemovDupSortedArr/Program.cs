5a8735e1497edf985d4df9e90c5cd2f4b47bc146
prashant153 <prashant153@gmail.com>
2018-04-13 14:36:51 +0530

    Removing duplicate numbers from sorted array using extra space (not an in-space solution)

---
+++ Arrays/RemovDupSortedArr/RemovDupSortedArr/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RemovDupSortedArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0,0,0, 1, 1, 1,3,3};
+            int[] retArray = new int[nums.Length];
+            Console.WriteLine("Before:");
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+            int wrtr = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (!(nums[i] == nums[i + 1]))
+                {
+                    retArray[wrtr++] = nums[i];
+                }
+            }
+            retArray[wrtr++] = nums[nums.Length - 1];
+            Console.WriteLine("After:");
+            for (int i = 0; i < wrtr; i++)
+            {
+                Console.WriteLine(retArray[i]);
+            }
+        }
+    }
+}
