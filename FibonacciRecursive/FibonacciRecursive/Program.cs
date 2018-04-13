dbcb9a03f767a88863a62e14906284153e361089
prashant153 <prashant153@gmail.com>
2018-04-13 11:57:43 +0530

    Returns nth value fibonacci number(Not Sequence) using recursion

---
+++ FibonacciRecursive/FibonacciRecursive/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FibonacciRecursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of N:");
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine(Fibonacci(number));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong!! Did you input a " +
+                                  "proper number??");
+            }
+        }
+
+        private static int Fibonacci(int number)
+        {
+            if (number<=1)
+            {
+                return number;
+            }
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+        }
+    }
+}
